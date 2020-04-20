836179bfc52c61a813ca5fc014a6dc4d3082303d
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-04-20 13:42:53 +0300

    add lesion detection common lib, use it in listen to generate attention and detection volumes

--- listen.py
+++ listen.py
@@ -1,51 +1,16 @@
 from common.utils import *
 from common import listener_server
 import os
-import subprocess as sb
+from lesion_detector_common.common import *
 
 output_dir = "covid_lesion_detection_output"
 output_path = os.path.join(os.environ["DATA_SHARE_PATH"], output_dir)
 
-def covid_detector(param_dict):
+def __get_attention_and_detection_volumes(files):
 
-    rel_source_file = param_dict["source_file"][0]
-    data_share = os.environ["DATA_SHARE_PATH"]
-    source_file = os.path.join(data_share, rel_source_file)
-
-    script_path = "/app/code/keras_retinanet/bin/predict_covid.py"
-    model_path = "/app/model/vgg19_csv_01.h5"
-
-    tmp = "/tmp"
-    input_path = os.path.join(tmp, "input")
-
-    if not os.path.exists(input_path):
-        os.mkdir(input_path)
-
-    cp_cmd = "cp {} {}".format(source_file, input_path)
-    log_debug("Running", cp_cmd)
-    cp_exit_code = sb.call([cp_cmd], shell=True)
-    if cp_exit_code == 1:
-        return {}, False
-
-
-    lesion_detection_cmd = "cd /app/code && python3 {} --model={} --gpu=0 --save-path={} nii {}"\
-                            .format(script_path, model_path, output_path, input_path)
-
-    log_debug("Running", lesion_detection_cmd)
-    exit_code = sb.call([lesion_detection_cmd], shell=True)
-    if exit_code == 1:
-        return {}, False
-
-    rm_tmp_input_cmd = "rm -rf {}".format(input_path)
-    log_debug("Running", rm_tmp_input_cmd)
-    rm_exit_code = sb.call([rm_tmp_input_cmd], shell=True)
-    if rm_exit_code == 1:
-        return {}, False
-
-    # get names of files
-    files = os.listdir(output_path)
     attention_volume = ""
     detection_volume = ""
+
     for file in files:
         name = os.path.split(file)[1]
 
@@ -54,18 +19,12 @@ def covid_detector(param_dict):
         elif "detection" in name:
             detection_volume = file
 
-    if attention_volume == "" or detection_volume == "":
-        return {}, False
+    return attention_volume, detection_volume
 
-    rel_attention_volume_path = os.path.join(output_dir, attention_volume.split()[1])
-    rel_detection_volume_path = os.path.join(output_dir, detection_volume.split()[1])
+def covid_detector(param_dict):
+    model_path = "/app/model/vgg19_csv_01.h5"
+    return covid_detector_base(param_dict, model_path, __get_attention_and_detection_volumes, output_dir)
 
-    result_dict = {
-        "attention_volume": rel_attention_volume_path,
-        "detection_volume": rel_detection_volume_path
-    }
-
-    return result_dict, True
 
 
 if __name__ == "__main__":
