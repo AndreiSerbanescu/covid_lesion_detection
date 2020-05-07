da7d9ea5d7df620fd7741354f874618254050579
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-05-07 20:15:08 +0300

    restructure project, make compatible with jip interface, slightly modify common lesion detection common lib

--- files/interface/listen.py
+++ files/interface/listen.py
@@ -23,8 +23,22 @@ def __get_attention_and_detection_volumes(files):
 
 def covid_detector(param_dict):
     model_path = "/app/model/vgg19_csv_01.h5"
-    return covid_detector_base(param_dict, model_path, __get_attention_and_detection_volumes, output_dir)
+    rel_source_file = param_dict["source_file"][0]
+    data_share = os.environ["DATA_SHARE_PATH"]
 
+    # remove trailing / at the beggining of name
+    # otherwise os.path.join has unwanted behaviour for base dirs
+    # i.e. join(/app/data_share, /app/wrongpath) = /app/wrongpath
+    rel_source_file = rel_source_file.lstrip('/')
+
+    source_file = os.path.join(data_share, rel_source_file)
+
+    return covid_detector_base(source_file, model_path, __get_attention_and_detection_volumes, output_dir)
+
+
+def covid_detector_absolute(source_file):
+    model_path = "/app/model/vgg19_csv_01.h5"
+    return covid_detector_base(source_file, model_path, __get_attention_and_detection_volumes, output_dir)
 
 
 if __name__ == "__main__":
@@ -35,11 +49,6 @@ if __name__ == "__main__":
     served_requests = {
         "/covid_detector_nifti": covid_detector
     }
-
-    #make output dir
-    
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
 
     listener_server.start_listening(served_requests, multithreaded=True, mark_as_ready_callback=mark_yourself_ready)
 
